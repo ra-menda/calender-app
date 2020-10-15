8b30d0651d55ca4f629de9d2bcdf387b611f72f5
ra-men <ramenda1217@gmail.com>
2020-10-16 00:43:06 +0900

    Macで編集

--- calender-app/src/App.js
+++ calender-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ハローワールド！！！ようこそ世界！！！これが見えるってことはきっと動いてるわ
+          Macで編集
         </p>
         <a
           className="App-link"
